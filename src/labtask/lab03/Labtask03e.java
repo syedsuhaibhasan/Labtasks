3f3a330bf503a71d86bab26588477a3cb4bf5c98
Syed Muhammad Suhaib Hasan <syedsuhaibhasan888@gmail.com>
2025-11-18 23:40:04 +0500

    commit

--- src/labtask/lab03/Labtask03e.java
+++ src/labtask/lab03/Labtask03e.java
@@ -1,12 +1,30 @@
 package labtask.lab03;
-public class Labtask03e extends InfixToPostfix{
-    public static void main(String[] args) {
-        String str = "A+B/C*(E-D)+C^F*Z";
-        Labtask03e obj = new Labtask03e(str.length());
-        obj.infixToPostfix(str);
+import java.util.*;
+
+public class Labtask03e {
+    public static int eval(String exp) {
+        Stack<Integer> st = new Stack<>();
+        String[] arr = exp.split(" ");
+        for (String s : arr) {
+            if (s.matches("-?\\d+")) {
+                st.push(Integer.parseInt(s));
+            } else {
+                int b = st.pop();
+                int a = st.pop();
+                int r = 0;
+                if (s.equals("+")) r = a + b;
+                else if (s.equals("-")) r = a - b;
+                else if (s.equals("*")) r = a * b;
+                else if (s.equals("/")) r = a / b;
+                st.push(r);
+            }
+        }
+        return st.pop();
     }
 
-    public Labtask03e(int size) {
-        super(size);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String x = sc.nextLine();
+        System.out.println(eval(x));
     }
 }
